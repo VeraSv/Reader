2460092693c6601c70fd4b68803a099811cb43d0
VeraSv <vera.svistun05@gmail.com>
2021-08-19 20:05:46 +0300

    add commands

--- Program.cs
+++ Program.cs
@@ -1,42 +1,95 @@
 ﻿using System;
 using System.IO;
 using System.Text.RegularExpressions;
+
+
 namespace Reader
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //System.IO.File file = new File();
-            Console.Write("Enter the path to the file: ");
+
+            Console.Write("Enter the Path: ");
             string path = Console.ReadLine();
-           
             bool exist = File.Exists(path);
-            
-            if (exist) {
-                Console.Write("Enter a word: ");
-                string word = Console.ReadLine();
-                word = word.ToLower().Trim();
-                string readText = File.ReadAllText(path);
-                int counter = 0;
-                Regex regex = new Regex(word, RegexOptions.IgnoreCase);
-                MatchCollection matches = regex.Matches(readText);
-                if (matches.Count > 0)
+
+            if (exist)
+            {
+
+                while (true)
                 {
-                    foreach (Match match in matches) counter++;
-                    Console.Write($"the word occurs {counter} times");
+                    Console.Write("Enter a word: ");
+                    string word = Console.ReadLine();
+                    word = word.ToLower().Trim();
+                    string readText = File.ReadAllText(path);
+                    Console.Write("Enter a command: \n f-find word \n r- replace word \n d- delete word \n ");
+                    string command = Console.ReadLine();
+                    Regex regex = new Regex(word, RegexOptions.IgnoreCase);
+                    switch (command)
+                    {
+                        case "f":
+                            findWord(readText, word, regex);
+                            break;
+                        case "r":
+                            Console.Write("Enter replace: ");
+                            string replace = Console.ReadLine();
+                            replaceWord(readText, word, replace, path, regex);
+                            break;
+                        case "d":
+                            deleteWord(readText, word, path, regex);
+                            break;
+                        default:
+                            Console.Write("UnKnown command  \n");
+
+                            break;
+                    }
                 }
-                else
-                {
-                    Console.WriteLine("No matches found");
-                }
-              
             }
             else
             {
-                Console.Write("File is not exist");
-            }
+                Console.Write("File is not exist \n");
 
+            }
+        }
+
+        static void findWord(string readText, string word, Regex regex)
+        {
+
+            int counter = 0;
+           
+            MatchCollection matches = regex.Matches(readText);
+            if (matches.Count > 0)
+            {
+                foreach (Match match in matches) counter++;
+                Console.Write($"the word occurs {counter} times  \n");
+            }
+            else
+            {
+                Console.Write("No matches found  \n");
+            }
+        }
+
+        static void replaceWord(string readText, string word, string replace, string path, Regex regex)
+        {
+            readText = regex.Replace(readText, replace);
+            
+            using (StreamWriter file = new StreamWriter(path))
+            {
+                file.Write(readText);
+            }
+            Console.Write("Replaced successfully  \n");
+        }
+        static void deleteWord(string readText, string word,string path, Regex regex)
+        {
+            readText = regex.Replace(readText, "");
+            using (StreamWriter file = new StreamWriter(path))
+            {
+                file.Write(readText);
+            }
+            Console.Write("Deleted successfully \n");
         }
     }
+
 }
+
